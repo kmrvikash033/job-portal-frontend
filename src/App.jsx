184856772e2c10b4dc4ff4e1b6a676ed6a7109ba
Vikash <kmr.vikash033@gmail.com>
2024-07-31 20:26:28 +0530

    initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {  Routes, Route } from "react-router-dom"
+import Home from "./pages/home"
+import Register from "./pages/register"
+
+
+function App() {
+  return (
+   <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/register" element={<Register/>} />
+   </Routes>
+  )
+}
+
+export default App
